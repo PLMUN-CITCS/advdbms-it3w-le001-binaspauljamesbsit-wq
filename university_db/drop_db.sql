93c0d6924294a14cd3e0e8f05af5575fc782d582
binaspauljamesbsit-wq <binaspauljames_bsit@plmun.edu.ph>
2025-09-16 19:30:01 +0800

    Delete university_db directory

--- university_db/drop_db.sql
+++
@@ -1,5 +0,0 @@
--- Step 3: (Optional) Drop the database if needed. 
--- Use with extreme caution!
-
--- The IF EXISTS clause prevents an error if the database doesn't exist.
-DROP DATABASE IF EXISTS `UniversityDB`;
